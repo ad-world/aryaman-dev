f8b577bbf93402cc5774ecbcdf2fc09e60d1472c
Aryaman Dhingra <aryamandhingra@gmail.com>
2021-03-16 10:23:07 +0400

    added typewriter effect

--- src/About.js
+++ src/About.js
@@ -3,12 +3,13 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 export default function About() {
     return (
-        <Container fluid id="about" style={{height:"100vh"}}>
-            <Container>
+        <Container fluid id="about" style={{height:"50vh"}}>
+            <Container >
                 <div className="align-items-center text-center">
                     <h1 className="mt-5 pt-5">About Me</h1>
                 </div>
-                <Row className="justify-content-around align-items-center text-center mt-5">
+                <Row className="justify-content-around align-items-center text-center mt-5 align-middle d-flex"
+                >
                     <Col className="col-md-3">I have project experience in Full-stack Web Development, Software Development, and
                     Machine Learning.</Col>
                     <Col className="col-md-3">I am currently in my 1B term studying Honors Computer Science @ University of
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
+import TypeWriterEffect from "react-typewriter-effect";
 
 export default function Header() {
     return (
-        <Container style={{height:"100vh"}}>
+        <Container style={{ height: "100vh" }}>
             <Row className="row justify-content-center align-items-center text-center">
                 <div className="col-12 d-flex align-items-center align-middle"
                     style={{
@@ -12,11 +13,21 @@ export default function Header() {
                     }}>
                     <Col >
                         <h1 className="text-black">
-                            Aryaman Dhingra
-                </h1>
+                            <TypeWriterEffect
+                            text="Aryaman Dhingra"
+                            hideCursor={true}
+                            hideCursorAfterText={true}
+                            textStyle={{textAlign:'center'}}
+                            />
+                        </h1>
                         <p className="text-black">
-                            Full-stack Web Developer/Data Scientist
-                </p>
+                            <TypeWriterEffect
+                            text="Full-stack Web Developer/Data Scientist"
+                            hideCursorAfterText={true}
+                            startDelay={2000}
+                            textStyle={{textAlign:'center', fontSize: '15px'}}
+                            />
+                        </p>
                     </Col>
                 </div>
             </Row>
