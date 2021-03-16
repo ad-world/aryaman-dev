8bed63b42203946e2911072e7511753c882e2a9c
Aryaman Dhingra <aryamandhingra@gmail.com>
2021-03-16 10:23:25 +0400

    created projects component - not ready yet

---
+++ src/Projects.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Projects(){
+    return (
+        <Container style={{height:"100vh"}} id="projects">
+            <div className="align-items-center text-center">
+                <h1 className="text-black mt-5 pt-5">
+                    Projects
+                </h1>
+            </div>
+        </Container>
+    )
+}
