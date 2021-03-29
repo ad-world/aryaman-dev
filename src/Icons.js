b07eb754d7677968b83e1f3b62dd2b8afb34cbcf
Aryaman Dhingra <aryamandhingra@gmail.com>
2021-03-29 18:37:14 +0400

    more hosting garbage

--- src/Icons.js
+++ src/Icons.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { ReactComponent as Icon} from './svg/iconfinder_link_3671790.svg'
-import { ReactComponent as Down} from './svg/iconfinder_icon-ios7-arrow-down_211687.svg'
 
 export default function Icons(){
     return (
